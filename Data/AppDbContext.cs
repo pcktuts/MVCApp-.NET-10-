7dc888b9401989af5db6b3d1dd5427cff4e9c7cf
pckrishnadas88 <pckrishnadas88@gmail.com>
2026-03-19 10:55:48 +0530

    Connect ASP.NET MVC to Database (EF Core Setup Step by Step) | Student Course Management System
    dotnet add package Microsoft.EntityFrameworkCore.SqlServer
    dotnet add package Microsoft.EntityFrameworkCore.Tools
    
    dotnet ef migrations add InitialCreate
    
    dotnet ef database update
    
    Install dotnet cli if not done
    
    dotnet tool install --global dotnet-ef

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCApp.Model;
+
+namespace MVCApp.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+       
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using MVCApp.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
-var app = builder.Build();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("Default")));
 
+var app = builder.Build();
 app.UseRouting();
 app.MapControllerRoute(
     name: "default",
