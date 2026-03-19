69ed9c7eeb98f9cee95f2b305b2c994ec559f8ba
pckrishnadas88 <pckrishnadas88@gmail.com>
2026-03-19 07:58:50 +0530

    Handle req with controllers, creating controller actions

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hello world from Action Index";
+        }
+        public string About() {
+            return "This is about page";
+        }
+    }
+}
